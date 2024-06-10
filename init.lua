f7ffc6d09e61c16f6b785c561b32af77d87467fa
Felix Hofmann <felix_hofmann_13@hotmail.com>
2024-06-10 23:02:30 +0200

    Uncomment custom plugins

--- init.lua
+++ init.lua
@@ -159,7 +159,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'everforest',
         component_separators = '|',
         section_separators = '',
       },
@@ -220,7 +220,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
